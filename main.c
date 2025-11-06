9d414d0c6f459465be9f58e216022f5311b71cbd
chaeeunkim24 <chae020703@sookmyung.ac.kr>
2025-11-06 16:00:07 +0900

    practice_6

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void){
+int main(void) {
     char input[100];
     FILE*fp;
+    char c;
 
-    int i;
+    fp = fopen("sample.txt", "r");
 
-    fp = fopen("sample.txt", "w");
-    for (i=0 ; i<3; i++){
-        printf("input a word: ");
-        scanf("%s", input);
-        fprintf(fp, "%s\n", input);
+    if (fp == NULL)
+        printf("파일을 못열음\n");
+
+     while ((c = fgetc(fp)) != EOF) {
+        putchar(c);
     }
-    
-    fclose(fp);
 
+    fclose(fp);
+    
+    system("PAUSE");
     return 0;
 }
